8655c4e89f4d5a9851a751b94270d4c84995638c
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2020-03-08 15:16:19 +0530

    App Interface Completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
 
     public static void main(String[] args) {
+        launch();
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
 
