4578a89795e77cb2d60eeb6fdceb9457d80a2165
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2020-03-17 07:44:11 +0530

    SignUpIn alert windows done

--- src/main/java/Controllers/SignInSignUpController.java
+++ src/main/java/Controllers/SignInSignUpController.java
@@ -5,19 +5,13 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class SignInSignUpController{
     @FXML
@@ -40,9 +34,31 @@ public class SignInSignUpController{
     @FXML
     private JFXButton btnSignUp;
 
+    @FXML
+    private JFXTextField textSignInName;
+    @FXML
+    private JFXPasswordField textSignInPassword;
+    @FXML
+    private JFXButton btnSignIn;
+
     private Parent root;
     private Stage stage;
     private Scene scene;
+
+    public void showAlert(String message) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../AlertWindow.fxml"));
+        root= loader.load();
+        scene = new Scene(root);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        AlertWindowController alertWindowController = loader.getController();
+        alertWindowController.setMessage(message);
+
+        stage.show();
+    }
 
     public void clickClose(MouseEvent mouseEvent) {
         System.exit(0);
@@ -57,23 +73,26 @@ public class SignInSignUpController{
     }
 
     public void handleSignUp(MouseEvent mouseEvent) throws Exception {
-        if(textSignUpName.getText().equals("")){
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../AlertWindow.fxml"));
-            root= loader.load();
-            scene = new Scene(root);
-            stage = new Stage();
-            stage.setScene(scene);
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.UNDECORATED);
-
-            AlertWindowController alertWindowController = loader.getController();
-            alertWindowController.setMessage("please enter User Name");
-
-            stage.show();
-
-
-
+        if(textSignUpName.getText().equals("") && textSignUpPassword.getText().equals("") && textSignUpConfPassword.getText().equals("")) {
+            showAlert("Please enter reqiured data");
         }
-        System.out.println(textSignUpName.getText());
+        else if(textSignUpName.getText().contains(" ")){
+            showAlert("User name cannot be contained spaces");
+        }
+        else if(!textSignUpPassword.getText().equals(textSignUpConfPassword.getText())){
+            showAlert("Password and Confirm Password mismatch");
+        }
+        else if(textSignUpPassword.getText().length()<7){
+            showAlert("Password is too short");
+        }
+        else{
+            
+        }
+    }
+
+    public void handleSignIn(MouseEvent mouseEvent) throws Exception {
+        if(textSignInName.getText().equals("") && textSignInPassword.getText().equals("")) {
+            showAlert("Please enter reqiured data");
+        }
     }
 }
