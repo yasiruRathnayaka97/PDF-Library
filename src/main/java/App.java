08b61aeac885fe1d34e1119925a70daab4d9933a
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2020-03-18 11:18:49 +0530

    Common store app loading completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,9 @@
+import Models.CommonStore;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.StageStyle;
+
 
 public class App extends Application {
     private Parent root;
@@ -14,34 +12,18 @@ public class App extends Application {
     private double xOffset = 0;
     private double yOffset = 0;
 
+    private CommonStore commonStore;
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        //load sign in sign up stage
-        root = FXMLLoader.load(getClass().getResource("SignInSignUp.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        scene = new Scene(root);
-        primaryStage.setScene(scene);
+        commonStore = CommonStore.getInstance();
 
-        //set stage movable
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
-        });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                primaryStage.setX(event.getScreenX() - xOffset);
-                primaryStage.setY(event.getScreenY() - yOffset);
-            }
-        });
-        primaryStage.show();
+        //load sign in sign up stage
+        commonStore.stageLoader("../SignInSignUp.fxml",true,null);
     }
 }
 
