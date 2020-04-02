cfcd6f463501d69ed7fe340b8b7b5b55db3bcd55
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2020-04-02 18:32:29 +0530

    Before restructuring

--- src/main/java/Models/CommonStore.java
+++ src/main/java/Models/CommonStore.java
@@ -8,9 +8,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import javax.naming.ldap.PagedResultsControl;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CommonStore {
